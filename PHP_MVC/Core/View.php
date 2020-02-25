0059bec838223f58373a31b3c50e8d2a9694435b
Darshan Tandel <darshantandel1998@gmail.com>
2020-02-25 11:23:50 +0530

    commit

---
+++ PHP_MVC/Core/View.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Core;
+
+use App\Config;
+
+class View
+{
+    public static function render($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
+        $file = "../App/Views/$view.html.twig";
+        if (is_readable($file))
+            require $file;
+        else
+            throw new \Exception("'$file' not found");
+        unset($_SESSION['error']);
+    }
+
+    public static function renderTemplate($template, $args = [])
+    {
+        static $twig = null;
+        if ($twig === null) {
+            $loader = new \Twig\Loader\FilesystemLoader('../App/Views');
+            $twig = new \Twig\Environment($loader);
+            $function = new \Twig\TwigFunction('static', function($path) {
+                return Config::BASE_DIR . $path;
+            });
+            $twig->addFunction($function);
+            $twig->addGlobal('GET', $_GET);
+            $twig->addGlobal('POST', $_POST);
+            $twig->addGlobal('SESSION', $_SESSION);
+            $twig->addGlobal('COOKIE', $_COOKIE);
+        }
+        $template .= '.html.twig';
+        echo $twig->render($template, $args);
+        unset($_SESSION['error']);
+    }
+}
